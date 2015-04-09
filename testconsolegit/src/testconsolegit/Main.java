7b9f6be8ddce58e8c9d2e0574c2566dfd2c29cd8
fisnikb <fisnikbullatovci@gmail.com>
2015-04-09 18:13:57 +0200

    Initial Test

---
+++ testconsolegit/src/testconsolegit/Main.java
@@ -0,0 +1,22 @@
+package testconsolegit;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+
+		displaymessage();
+		
+		
+	}
+	private static void displaymessage()
+	{
+      for (int i = 0; i < 10; i++) {
+    	  System.out.println("Git" + i);
+    	  
+	}
+				
+	}
+
+}
